dcaf8843a0a589f35ebfac77da6a8e3e4ef9d2ce
sayed_hossain <sayed.hasan0011@gmail.com>
2023-10-03 23:51:55 +0600

    Create a dynamic responsive navbar

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,28 @@ import {
 } from "react-router-dom";
 import './index.css'
 import Root from '../src/Root'
+import Home from './components/Home';
+import Register from './components/Register';
+import Login from './components/Login';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root></Root>
+    element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      }
+    ]
   },
 ]);
 
